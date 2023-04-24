5f621d2582da8696b95f43c880ddf1051b24ac00
Benjamin <benjaminevans316@gmail.com>
2023-04-24 13:42:33 +0200

    Minor improvements

--- test_nn_model.py
+++ test_nn_model.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn 
 import torch.optim as optim
 import torch.nn.functional as F
-
+from numba import njit
 
 def MBE(y_true, y_pred):
     '''
@@ -60,7 +60,6 @@ class MyNet(nn.Module):
         self.loss_function = nn.MSELoss()
         self.optimizer = optim.Adam(self.parameters(), lr=0.001)
         
-
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
@@ -108,17 +107,17 @@ def test_nn_model_loss(model, x, y):
     return np.mean(loss)
 
 
-def train_nn_model(x_train, y_train, x_test, y_test):
+def train_nn_model(x_train, y_train, x_test, y_test, epochs):
     model = MyNet()
     
     train_losses, test_losses = [], []
     
-    for i in range(5):
-        x, y = select_mini_batch(x_train, y_train, 100)
+    for i in range(epochs):
+        x, y = select_mini_batch(x_train, y_train, 200)
         
         outputs, loss = model.train_model(x, y)
     
-        print(f"Epoch {i+1} - loss: {loss:.4f}")
+        print(f"Epoch {i+1} - loss: {loss:.6f}")
         train_losses.append(loss)
         test_loss = test_nn_model_loss(model, x_test, y_test)
         test_losses.append(test_loss)
@@ -145,53 +144,20 @@ def run_simulation_torch(model, scalery, x_test):
     for i in range(12):
         outputs = model.test_model(model_inputs) # (N-12, 1)
         predicted_temperature_set = scalery.inverse_transform(outputs)[:, 0] # (N-12)
+        tempterature_errors[:, i] = np.sqrt((true_temperatures[i:N-(12-i), 0] - predicted_temperature_set)**2)
         predicted_tempteratures[:, i] = predicted_temperature_set
-        true_values = true_temperatures[i:N-(12-i), 0]
-        predictions = predicted_temperature_set
-        errors = np.sqrt((predictions - true_values)**2)
-        tempterature_errors[:, i] = errors
-        # tempterature_errors[:, i] = math.sqrt((true_temperatures[i:N-(12-i)] - predicted_temperature_set)**2)
         
         fan_modes = fan_logic_array(model_inputs[:, 3], predicted_temperature_set)
         model_inputs = x_test[i+1:N-(11-i), :]
         model_inputs[:, 2] = outputs[:, 0]
         model_inputs[:, 3] = fan_modes
         
-    fan_predictions = fan_modes # last prediction.
+    fan_predictions = fan_modes*5 # last prediction.
     predicted_internal_temp = predicted_temperature_set # last prediction.
-
     
     return predicted_internal_temp, tempterature_errors, fan_predictions
 
-
-
-def run_simulation_benjamin(model, scalery, X_test):
-    N = len(X_test)
-    
-    predicted_tempteratures = np.zeros((N-12,12))
-    fan_predictions = np.zeros(N-12)
-    tempterature_errors = np.zeros((N-12,12))
-    
-    for i in range(N-12): # steps through each element in test data
-        input_sim = X_test[i,:].reshape(1,-1)
-        
-        for j in range(12): # predicts 12x5 minute steps ahead
-            input_sim_tensor = torch.from_numpy(input_sim).float()
-            predicted_scaled_temp = model(input_sim_tensor).detach().numpy()
-            predicted_tempterature = scalery.inverse_transform(predicted_scaled_temp.reshape(1,-1))
-            predicted_tempteratures[i,j] = predicted_tempterature
-            tempterature_errors[i, j] = math.sqrt((true_temperatures[i+j] - predicted_tempterature)**2)
-    
-            fan_setting = fan_logic(input_sim[0, 3], predicted_tempterature)
-            input_sim = np.array([X_test[i+j+1,0], X_test[i+j+1,1], predicted_scaled_temp[0, 0], fan_setting]).reshape(1,-1)
-        
-        fan_predictions[i] = fan_setting * 5
-    
-    predicted_internal_temp = predicted_tempteratures[:, 11]
-    
-    return predicted_internal_temp, tempterature_errors, fan_predictions
-
-
+@njit(cache=True)
 def fan_logic_array(previous_fans, temperatures):
     new_fans = np.zeros(len(previous_fans))
     for i in range(len(previous_fans)):
@@ -199,7 +165,7 @@ def fan_logic_array(previous_fans, temperatures):
         
     return new_fans
 
-#TODO: njit this    
+@njit(cache=True)
 def fan_logic(previous_fan, temperature):
     if previous_fan == 1 and temperature > 22:
         new_fan_setting = 1
@@ -258,19 +224,15 @@ def plot_prediction_errors(error_array):
 
     
 if __name__ == "__main__":
-    
     x_train, y_train, x_test, y_test, scalery = load_data("Data/training.csv", "Data/TestSet.csv")
-    
-    model = train_nn_model(x_train, y_train, x_test, y_test)
+    model = train_nn_model(x_train, y_train, x_test, y_test, 200)
     
     true_temperatures = scalery.inverse_transform(y_test.reshape(-1, 1))
-    
     predicted_temperatures, error_array, fan_pred = run_simulation_torch(model, scalery, x_test)
+    
     calculate_metrics(predicted_temperatures, true_temperatures)
     plot_temperatrues(true_temperatures, predicted_temperatures, x_test, fan_pred)
     plot_prediction_errors(error_array)
-    predicted_temperatures, error_array, fan_pred = run_simulation_benjamin(model, scalery, x_test)
-    calculate_metrics(predicted_temperatures, true_temperatures)
     
     
     
